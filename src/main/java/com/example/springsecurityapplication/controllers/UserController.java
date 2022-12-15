113d904caa37522627ec2e42804b287661c364ed
satoshiLYN <kcashflow@yandex.ru>
2022-12-15 16:54:36 +0300

    front user(index)

--- src/main/java/com/example/springsecurityapplication/controllers/UserController.java
+++ src/main/java/com/example/springsecurityapplication/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.example.springsecurityapplication.models.Order;
 import com.example.springsecurityapplication.models.Product;
 import com.example.springsecurityapplication.repositories.CartRepository;
 import com.example.springsecurityapplication.repositories.OrderRepository;
+import com.example.springsecurityapplication.repositories.ProductRepository;
 import com.example.springsecurityapplication.security.PersonDetails;
 import com.example.springsecurityapplication.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +25,15 @@ import java.util.UUID;
 
 @Controller
 public class UserController {
+    private final ProductRepository productRepository;
 
     private final OrderRepository orderRepository;
     private final ProductService productService;
     private final CartRepository cartRepository;
 
     @Autowired
-    public UserController(OrderRepository orderRepository, ProductService productService, CartRepository cartRepository) {
+    public UserController(ProductRepository productRepository, OrderRepository orderRepository, ProductService productService, CartRepository cartRepository) {
+        this.productRepository = productRepository;
         this.orderRepository = orderRepository;
         this.productService = productService;
         this.cartRepository = cartRepository;
@@ -151,5 +156,74 @@ public class UserController {
         List<Order> orderList = orderRepository.findByPerson((personDetails.getPerson()));
         model.addAttribute("orders", orderList);
         return "/user/orders";
+    }
+
+    @PostMapping("index/search")
+    public String productSearch(@RequestParam("search") String search, @RequestParam("min") String min, @RequestParam("max") String max, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "category", required = false, defaultValue = "") String category, Model model){
+//        System.out.println(search);
+//        System.out.println(min);
+//        System.out.println(max);
+//        System.out.println(price);
+//        System.out.println(category);
+        // если диапозон цен ОТ и ДО не пустой
+        if (!min.isEmpty() & !max.isEmpty()) {
+            // если сортировка по цене выбрана
+            if (!price.isEmpty()) {
+                // если в качестве сортировки выбрана сортировка по возрастанию
+                if (price.equals("sorted_by_ascending_price")) {
+
+                    // если категория товара не пустая
+                    if (!category.isEmpty()) {
+                        // если категория равна animal
+                        if (category.equals("animal")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 1));
+                        } else if (category.equals("feed")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 2));
+                        } else if (category.equals("terrarium")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 3));
+                        } else if (category.equals("equipment")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 4));
+                        } else if (category.equals("decor")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 5));
+                        }
+
+                        // если категория не выбрана
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPrice(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max)));
+                    }
+
+                    // если сортировка выбрана по убыванию
+                } else if (price.equals("sorted_by_descending_price")) {
+
+                    if (!category.isEmpty()) {
+
+                        if (category.equals("animal")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 1));
+                        } else if (category.equals("feed")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 2));
+                        } else if (category.equals("terrarium")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 3));
+                        } else if (category.equals("equipment")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 4));
+                        } else if (category.equals("decor")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max), 5));
+                        }
+
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max)));
+                    }
+                }
+            } else {
+                model.addAttribute("search_product", productRepository.findByTitleAndPriceGreaterThenEqualAndPriceLessThen(search.toLowerCase(), Float.parseFloat(min), Float.parseFloat(max)));
+            }
+
+        } else {
+            model.addAttribute("search_product", productRepository.findByTitleContainingIgnoreCase(search));
+        }
+        model.addAttribute("value_search", search);
+        model.addAttribute("value_min", min);
+        model.addAttribute("value_max", max);
+//        model.addAttribute("products", productService.getAllProduct());
+        return "user/index";
     }
 }
